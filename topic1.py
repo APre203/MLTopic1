2bfe6734b5c566b2263ea8b4416c3d5fe5460eca
Andrew Presman <alpresma@buffalo.edu>
2023-08-30 02:13:38 -0400

    Created transcriber for task1

--- topic1.py
+++ topic1.py
@@ -1,8 +1,26 @@
 # need to have ffmpeg installed (Works on Google Colab)
 # need to have transformers installed
 from transformers import pipeline
+import os
+
+filename = r"C:\Users\andrew\Downloads\cv-corpus-13.0-delta-2023-03-09\en\clips" # this has to be the filename of the mainclips audio
+
+def multiple_transcriber(filename, numb, type): # transcribe multiple files given filepath of main file
+
+    audio_file_folder = filename
+
+    all_files = os.listdir(audio_file_folder)
+    audio_files = [file for file in all_files if file.endswith(type)][:numb]
+
+    for audio_file in audio_files:
+        audio_path = os.path.join(audio_file_folder, audio_file)
+        print(transcriber(audio_path))
+
 
 speech_recognizer = pipeline("automatic-speech-recognition", model="openai/whisper-large-v2")
 
-transcribe = speech_recognizer(r"C:\Users\andrew\Documents\research-ML\topic1-ML\name_andrew.wav", max_new_tokens=8000)
-print(transcribe['text'])
+def transcriber(filename): # transcribe audio given the filepath of an audio
+    transcribe = speech_recognizer(filename, max_new_tokens=8000)
+    return transcribe['text']
+
+multiple_transcriber(filename, 20, ".mp3")
